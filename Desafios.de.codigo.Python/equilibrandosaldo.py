4d0b59d18c902b2b628b9a8bffc5d9a93b9d099c
Carol <carolnerisfonseca@gmail.com>
2023-08-29 21:57:25 -0300

    Primeiro Ramo

---
+++ Desafios.de.codigo.Python/equilibrandosaldo.py
@@ -0,0 +1,14 @@
+saldo_atual = float(input())
+valor_deposito = float(input())
+valor_retirada = float(input())
+
+#TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
+
+#TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
+
+# Calcular o saldo atualizado de acordo com a descrição deste desafio.
+saldo_atual += valor_deposito
+saldo_atual -= valor_retirada
+
+# Imprimir a saída conforme a tabela de exemplos (uma casa decimal).
+print(f"Saldo atualizado na conta: {saldo_atual:.1f}")
